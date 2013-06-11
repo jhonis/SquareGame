1b66b8a17149206cd54bd19e4e79eef081bcb63b
Jhonis de Souza <jhonis.souza@gmail.com>
2013-06-11 15:33:24 -0300

    First step, including images on screen.

--- SquareGame/build/source/r/debug/com/x/squaregame/R.java
+++ SquareGame/build/source/r/debug/com/x/squaregame/R.java
@@ -11,17 +11,35 @@ public final class R {
     public static final class attr {
     }
 
+    public static final class color {
+        public static final int grid_background = 0x7f040000;
+    }
+
     public static final class dimen {
-        public static final int activity_horizontal_margin = 0x7f040000;
-        public static final int activity_vertical_margin = 0x7f040001;
+        public static final int activity_horizontal_margin = 0x7f050000;
+        public static final int activity_vertical_margin = 0x7f050001;
     }
 
     public static final class drawable {
         public static final int ic_launcher = 0x7f020000;
+        public static final int img1 = 0x7f020001;
+        public static final int img2 = 0x7f020002;
+        public static final int img3 = 0x7f020003;
+        public static final int img4 = 0x7f020004;
+        public static final int img5 = 0x7f020005;
+        public static final int img6 = 0x7f020006;
+        public static final int img7 = 0x7f020007;
+        public static final int img8 = 0x7f020008;
     }
 
     public static final class id {
-        public static final int action_settings = 0x7f080000;
+        public static final int action_settings = 0x7f0a0002;
+        public static final int imageGridView = 0x7f0a0000;
+        public static final int imageHolder = 0x7f0a0001;
+    }
+
+    public static final class integer {
+        public static final int num_columns = 0x7f060000;
     }
 
     public static final class layout {
@@ -29,13 +47,13 @@ public final class R {
     }
 
     public static final class menu {
-        public static final int main = 0x7f070000;
+        public static final int main = 0x7f090000;
     }
 
     public static final class string {
-        public static final int action_settings = 0x7f050000;
-        public static final int app_name = 0x7f050001;
-        public static final int hello_world = 0x7f050002;
+        public static final int action_settings = 0x7f070000;
+        public static final int app_name = 0x7f070001;
+        public static final int hello_world = 0x7f070002;
     }
 
     public static final class style {
@@ -47,10 +65,10 @@ public final class R {
          * API 11 theme customizations can go here.
          * API 14 theme customizations can go here.
          */
-        public static final int AppBaseTheme = 0x7f060000;
+        public static final int AppBaseTheme = 0x7f080000;
         /**
          * All customizations that are NOT specific to a particular API-level can go here.
          */
-        public static final int AppTheme = 0x7f060001;
+        public static final int AppTheme = 0x7f080001;
     }
 }
--- SquareGame/src/main/java/com/x/squaregame/MainActivity.java
+++ SquareGame/src/main/java/com/x/squaregame/MainActivity.java
@@ -2,22 +2,65 @@ package com.x.squaregame;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.view.Menu;
+import android.util.Log;
+import android.widget.ArrayAdapter;
+import android.widget.GridView;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+/**
+ * This activity presents a screen with a grid on which images can be added and moved around.
+ * It also defines areas on the screen where the dragged views can be dropped. Feedback is
+ * provided to the user as the objects are dragged over these drop zones.
+ * <p/>
+ * <p> Like the DragActivity in the previous version of the DragView example application, the
+ * code here is derived from the Android Launcher code.
+ * <p/>
+ * <p> The original Launcher code required a long click (press) to initiate a drag-drop sequence.
+ * If you want to see that behavior, set the variable mLongClickStartsDrag to true.
+ * It is set to false below, which means that any touch event starts a drag-drop.
+ */
 
 public class MainActivity extends Activity {
 
-    @Override
+    private ArrayAdapter<ImageView> adapterTest;
+
+    public static final boolean Debugging = false;
+
+    public void addNewImageToScreen(int resourceId) {
+        ImageView newView = new ImageView(this);
+        newView.setImageResource(resourceId);
+        GridView grid = (GridView) findViewById(R.id.imageGridView);
+        grid.setAdapter(new ImageAdapter(this));
+    }
+
+    public void addImagesToScreen() {
+
+        addNewImageToScreen(R.drawable.img1);
+//        addNewImageToScreen(R.drawable.img2);
+//        addNewImageToScreen(R.drawable.img3);
+//        addNewImageToScreen(R.drawable.img4);
+//        addNewImageToScreen(R.drawable.img5);
+//        addNewImageToScreen(R.drawable.img6);
+//        addNewImageToScreen(R.drawable.img7);
+//        addNewImageToScreen(R.drawable.img8);
+    }
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        adapterTest = new ArrayAdapter<ImageView>(this, android.R.layout.simple_list_item_1);
+        addImagesToScreen();
     }
 
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
+    public void toast(String msg) {
+        Toast.makeText(getApplicationContext(), msg, Toast.LENGTH_SHORT).show();
     }
 
+    public void trace(String msg) {
+        Log.d("DragActivity", msg);
+        if (!Debugging) return;
+        toast(msg);
+    }
 }
