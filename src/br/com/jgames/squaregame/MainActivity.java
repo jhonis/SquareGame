7894113c449be7001c5914448028a5b34a3cc176
Jhonis de Souza <jhonis.souza@gmail.com>
2013-09-11 14:16:38 -0300

    Implementacao inicial

--- src/br/com/jgames/squaregame/MainActivity.java
+++ src/br/com/jgames/squaregame/MainActivity.java
@@ -1,15 +1,10 @@
 package br.com.jgames.squaregame;
 
 import android.app.Activity;
-import android.content.Context;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
-import android.widget.AbsoluteLayout;
-import android.widget.ImageView;
-import android.widget.LinearLayout;
-import android.widget.RelativeLayout;
+import android.widget.Button;
 import br.com.jgames.squaregame.views.TouchImageView;
 
 public class MainActivity extends Activity {
@@ -21,47 +16,18 @@ public class MainActivity extends Activity {
         super.onCreate(bundle);
         setContentView(R.layout.main);
 
-        RelativeLayout root = (RelativeLayout) findViewById(R.id.main_layout);
-        Context ctx = getApplicationContext();
-
-        TouchImageView img1 = new TouchImageView(ctx, R.drawable.img1, 0, 0);
-        img1.setOnTouchListener(new View.OnTouchListener() {
+        Button btn = (Button) findViewById(R.id.btn_novo_jogo);
+        btn.setOnTouchListener(new View.OnTouchListener() {
             @Override
             public boolean onTouch(View view, MotionEvent motionEvent) {
-                Log.i(CATEGORIA, "teste dddd!! ");
+                if (motionEvent.getAction() == MotionEvent.ACTION_DOWN) {
+                    TouchImageView touchImageView = (TouchImageView) findViewById(R.id.touch_image_view);
+                    touchImageView.startGame();
+                    touchImageView.invalidate();
+                    return true;
+                }
                 return false;
             }
         });
-        root.addView(img1);
-
-        TouchImageView img2 = new TouchImageView(ctx, R.drawable.img2, 230, 0);
-        root.addView(img2);
-
-        TouchImageView img3 = new TouchImageView(ctx, R.drawable.img3, 460, 0);
-        root.addView(img3);
-
-        TouchImageView img4 = new TouchImageView(ctx, R.drawable.img4, 0, 230);
-        root.addView(img4);
-
-        TouchImageView img5 = new TouchImageView(ctx, R.drawable.img5, 230, 230);
-        root.addView(img5);
-
-        TouchImageView img6 = new TouchImageView(ctx, R.drawable.img6, 460, 230);
-        root.addView(img6);
-
-        TouchImageView img7 = new TouchImageView(ctx, R.drawable.img7, 0, 460);
-        root.addView(img7);
-
-        TouchImageView img8 = new TouchImageView(ctx, R.drawable.img8, 230, 460);
-        root.addView(img8);
-        img8.setOnTouchListener(new View.OnTouchListener() {
-            @Override
-            public boolean onTouch(View view, MotionEvent motionEvent) {
-                Log.i(CATEGORIA, "teste yyyyy!! ");
-                return false;
-            }
-        });
-
-        Log.i(CATEGORIA, "xxxxxx");
     }
 }
--- src/br/com/jgames/squaregame/views/TouchImageView.java
+++ src/br/com/jgames/squaregame/views/TouchImageView.java
@@ -2,26 +2,113 @@ package br.com.jgames.squaregame.views;
 
 import android.content.Context;
 import android.graphics.Canvas;
-import android.graphics.Paint;
 import android.graphics.Rect;
 import android.graphics.drawable.Drawable;
+import android.util.AttributeSet;
 import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
+import android.widget.Toast;
+import br.com.jgames.squaregame.R;
+
+import java.util.*;
 
 public class TouchImageView extends View {
     private static final String CATEGORIA = "squareGame";
-    private Drawable img;
-    int x, y;
+    private Context context;
+    private Map<Integer, Drawable> images;
+    private List<Rect> coords;
+    private Integer[] positions;
+    int x, y, index, index_none, magic, d_size;
     private boolean selecionou;
 
-    public TouchImageView(Context context, int idImagem, int x, int y) {
-        super(context);
-        this.x = x;
-        this.y = y;
+    public TouchImageView(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+        this.context = context;
+        startGame();
+    }
 
-        img = context.getResources().getDrawable(idImagem);
-//        setFocusable(true);
+    public TouchImageView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        this.context = context;
+        startGame();
+    }
+
+    public TouchImageView(Context context) {
+        super(context);
+        this.context = context;
+        startGame();
+    }
+
+    public void startGame() {
+        coords = new ArrayList<Rect>();
+        coords.add(new Rect(0, 0, 150, 150));
+        coords.add(new Rect(150, 0, 300, 150));
+        coords.add(new Rect(300, 0, 450, 150));
+        coords.add(new Rect(0, 150, 150, 300));
+        coords.add(new Rect(150, 150, 300, 300));
+        coords.add(new Rect(300, 150, 450, 300));
+        coords.add(new Rect(0, 300, 150, 450));
+        coords.add(new Rect(150, 300, 300, 450));
+        coords.add(new Rect(300, 300, 450, 450));
+
+        positions = new Integer[9];
+        for (int i = 0; i < positions.length; i++) {
+            positions[i] = i;
+        }
+        random();
+        fillImages();
+        d_size = 150;
+        magic = (int) Math.sqrt(images.size());
+        setFocusable(true);
+    }
+
+    private void fillImages() {
+        images = new LinkedHashMap<Integer, Drawable>();
+        for (int i = 0; i < positions.length; i++) {
+            Drawable temp;
+            switch (i) {
+                case 0:
+                    temp = context.getResources().getDrawable(R.drawable.img1);
+                    break;
+                case 1:
+                    temp = context.getResources().getDrawable(R.drawable.img2);
+                    break;
+                case 2:
+                    temp = context.getResources().getDrawable(R.drawable.img3);
+                    break;
+                case 3:
+                    temp = context.getResources().getDrawable(R.drawable.img4);
+                    break;
+                case 4:
+                    temp = context.getResources().getDrawable(R.drawable.img5);
+                    break;
+                case 5:
+                    temp = context.getResources().getDrawable(R.drawable.img6);
+                    break;
+                case 6:
+                    temp = context.getResources().getDrawable(R.drawable.img7);
+                    break;
+                case 7:
+                    temp = context.getResources().getDrawable(R.drawable.img8);
+                    break;
+                default:
+                    temp = context.getResources().getDrawable(R.drawable.img_none);
+                    temp.setVisible(false, false);
+                    index_none = positions[i];
+            }
+            images.put(positions[i], temp);
+        }
+    }
+
+    private void random() {
+        for (int i = 0; i < positions.length; i++) {
+            Random r = new Random();
+            int p = r.nextInt(positions.length);
+            int temp = positions[i];
+            positions[i] = positions[p];
+            positions[p] = temp;
+        }
     }
 
     @Override
@@ -38,33 +125,83 @@ public class TouchImageView extends View {
     public void onDraw(Canvas canvas) {
         super.onDraw(canvas);
 
-        Paint pincel = new Paint();
-        Rect r = new Rect(x, y, x + img.getIntrinsicWidth(), y + img.getIntrinsicHeight());
-        canvas.drawRect(r, pincel);
-        img.setBounds(r);
-        img.draw(canvas);
+        for (int i = 0; i < images.size(); i++) {
+            if (i != index || !selecionou) {
+                images.get(i).setBounds(coords.get(i));
+                images.get(i).draw(canvas);
+            }
+        }
+        if (selecionou && images.get(index).isVisible() && canChange(index)) {
+            images.get(index).setBounds(x, y, x + d_size, y + d_size);
+            images.get(index).draw(canvas);
+        }
+    }
+
+    public boolean canChange(int i) {
+        int magic_mod = index_none % magic;
+        return ((i == (index_none - magic)) || // posição superior ao espaço vazio
+                (i == (index_none + magic)) || // posição inferior ao espaço vazio
+                ((magic_mod == 2) && (i == index_none - 1)) || // posição esquerda ao espaço vazio caso ele esteja na borda direita
+                ((magic_mod == 0) && (i == index_none + 1)) || // posição direita ao espaço vazio caso ele esteja na borda esquerda
+                ((magic_mod == 1) && ((i == index_none + 1) || (i == index_none - 1)))); // posição direita e esquerda ao espaço vazio caso ele esteja no centro
+    }
+
+    public void checkWin() {
+        boolean resultado = true;
+        for (int i = 0; i < images.size(); i++) {
+            if (i != positions[i]) {
+                resultado = false;
+            }
+        }
+        if (resultado) {
+            Log.i(CATEGORIA, "Você ganhou!!!");
+            Toast.makeText(context, "Você ganhou!!!", Toast.LENGTH_SHORT).show();
+        }
     }
 
     @Override
     public boolean onTouchEvent(MotionEvent motionEvent) {
-        Log.i(CATEGORIA, "teste: "+ img.toString());
         float x = motionEvent.getX();
         float y = motionEvent.getY();
-        switch (motionEvent.getAction()) {
-            case MotionEvent.ACTION_DOWN:
-                selecionou = img.copyBounds().contains((int) x, (int) y);
-                break;
-            case MotionEvent.ACTION_MOVE:
-                if (selecionou) {
-                    this.x = (int) x - (img.getIntrinsicWidth() / 2);
-                    this.y = (int) y - (img.getIntrinsicHeight() / 2);
+        Rect r;
+        for (int i = 0; i < coords.size(); i++) {
+            r = coords.get(i);
+            if (r.contains((int) x, (int) y)) {
+                this.x = r.left;
+                this.y = r.top;
+                if (!selecionou) {
+                    index = i;
                 }
-                break;
-            case MotionEvent.ACTION_UP:
-                selecionou = false;
-                break;
+                switch (motionEvent.getAction()) {
+                    case MotionEvent.ACTION_DOWN:
+                        selecionou = true;
+                        break;
+                    case MotionEvent.ACTION_MOVE:
+                        this.x = (int) x - (d_size / 2);
+                        this.y = (int) y - (d_size / 2);
+                        break;
+                    case MotionEvent.ACTION_UP:
+                        if (images.get(index).isVisible() && !images.get(i).isVisible() && canChange(index)) {
+                            Drawable temp = images.get(i);
+                            images.put(i, images.get(index));
+                            images.put(index, temp);
+                            index_none = index;
+                            for (int j = 0; j < positions.length; j++) {
+                                if (positions[j] == i) {
+                                    positions[j] = index;
+                                } else if (positions[j] == index) {
+                                    positions[j] = i;
+                                }
+                            }
+                            checkWin();
+                        }
+                        selecionou = false;
+                        break;
+                }
+                invalidate();
+                return true;
+            }
         }
-        invalidate();
-        return true;
+        return false;
     }
 }
